08442ab03e3a8a6ee5e94e10a7cf62031fd5277d
Angie <kaitoribani@gmail.com>
2018-09-22 18:55:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var	countryList = $('#country');
+const countryList = $('#country');
 
 function searchCountry() {
-	var countryName = $('#country-name').val();
+	let countryName = $('#country-name').val();
 	if (!countryName.length) {
 		countryName = 'Poland';
 	}
 	$.ajax ({
-		url :  'https://restcountries.eu/rest/v1/name/' + countryName,
+		url :  `https://restcountries.eu/rest/v1/name/${countryName}`,
 		method : 'GET',
 		success : showCountryList,
 	});
@@ -14,19 +14,19 @@ function searchCountry() {
 
 function showCountryList(resp) {
 	countryList.empty();
-	resp.forEach(function(item) {
-		var $li = $('<li>').appendTo(countryList),
-			$name = $('<h3>').text(item.name + ' (' + item.nativeName + ') - ' + item.alpha2Code),
-			$spell = $('<p>' + 'Alternative spellings: '.bold() + item.altSpellings.join(',  ') + '</p>'),
-			$capital = $('<p>' + 'Capital: '.bold() + item.capital + '</p>'),
-			$region = $('<p>' + 'Region: '.bold() + item.subregion + '</p>'),
-			$population = $('<p>' + 'Population: '.bold() + item.population + ' people' + '</p>'),
-			$time = $('<p>' + 'Time zone: '.bold() + item.timezones + '</p>'),
-			$currencies = $('<p>' + 'Currency: '.bold() + item.currencies + '</p>'),
-			$area = $('<p>' + 'Area: '.bold() + item.area + ' km<sup>2</sup>' + '</p>'),
-			$callCode = $('<p>' + 'Calling code: '.bold() + '(+' + item.callingCodes + ')' + '</p>');	
-			$li.append($name).append($spell).append($capital).append($region).append($population).append($time).append($currencies).append($area).append($callCode);
-	});
+	resp.forEach(item => {
+        const $li = $('<li>').appendTo(countryList);
+        const $name = $('<h3>').text(`${item.name} (${item.nativeName}) - ${item.alpha2Code}`);
+        const $spell = $(`<p>${'Alternative spellings: '.bold()}${item.altSpellings.join(',  ')}</p>`);
+        const $capital = $(`<p>${'Capital: '.bold()}${item.capital}</p>`);
+        const $region = $(`<p>${'Region: '.bold()}${item.subregion}</p>`);
+        const $population = $(`<p>${'Population: '.bold()}${item.population} people</p>`);
+        const $time = $(`<p>${'Time zone: '.bold()}${item.timezones}</p>`);
+        const $currencies = $(`<p>${'Currency: '.bold()}${item.currencies}</p>`);
+        const $area = $(`<p>${'Area: '.bold()}${item.area} km<sup>2</sup></p>`);
+        const $callCode = $(`<p>${'Calling code: '.bold()}(+${item.callingCodes})</p>`);
+        $li.append($name).append($spell).append($capital).append($region).append($population).append($time).append($currencies).append($area).append($callCode);
+    });
 }
 
 $('#search').on('click', searchCountry);
